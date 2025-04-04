8037064389403a141a4f9821048edce20081ec71
lacheporter <114413176+lacheporter@users.noreply.github.com>
2025-04-04 14:46:24 +0000

    fixed some of the filtering

--- manage_stores.py
+++ manage_stores.py
@@ -3,49 +3,42 @@ import requests
 import pandas as pd
 import streamlit as st
 
-
 data = [
-        {"Store #": 440, "State": "New Jersey","Employees": 4, "Closed": True},
-        {"Store #": 430, "State": "South Carolina", "Employees": 5, "Closed": False},
-        {"Store #": 250, "State": "South Carolina", "Employees": 3, "Closed": True},
-        {"Store #": 360, "State": "South Carolina", "Employees": 3, "Closed": True},
-        {"Store #": 420, "State": "Georgia", "Employees": 6, "Closed": True},
-        {"Store #": 220, "State": "West Virginia", "Employees": 3, "Closed": True},
-        {"Store #": 5670, "State": "West Virginia", "Employees": 6, "Closed": True},
-        {"Store #": 489, "State": "Iowa", "Employees": 4, "Closed": True},
-        {"Store #": 440, "State": "North Carolina", "Employees": 6, "Closed": True},
+        {"Store": 440, "State": "New Jersey","Employees": 4, "Closed": True},
+        {"Store": 760, "State": "New Jersey","Employees": 4, "Closed": True},
+        {"Store": 430, "State": "South Carolina", "Employees": 5, "Closed": False},
+        {"Store": 250, "State": "South Carolina", "Employees": 3, "Closed": True},
+        {"Store": 360, "State": "South Carolina", "Employees": 3, "Closed": True},
+        {"Store": 420, "State": "Georgia", "Employees": 6, "Closed": True},
+        {"Store": 220, "State": "West Virginia", "Employees": 3, "Closed": True},
+        {"Store": 567, "State": "West Virginia", "Employees": 6, "Closed": True},
+        {"Store": 489, "State": "Iowa", "Employees": 4, "Closed": True},
+        {"Store": 440, "State": "North Carolina", "Employees": 6, "Closed": True},
     ]
-df = pd.DataFrame(data)
 
-if "visibility" not in st.session_state:
-    st.session_state.visibility = "visible"
-    st.session_state.disabled = True
-
-
-# Fedex Data (API)
 st.title("Fedex Store Management")
 
-option1 = st.selectbox(
-    "Location",
-    (df["State"]),
-)
-option2 = st.selectbox(
-    "Store #",
-    (df["Store #"]),
-    disabled=True
-)
+#Create a dataframe...
+df = pd.DataFrame(data)
+
+col1, col2 = st.columns([2, 1])
+#Create Select box of States, drop duplicates...
+states = df["State"].drop_duplicates()
+with col1:
+    option1 = st.selectbox("Location",(states),key=1)
+
+#Create select box of Stores based on State choice...
+stores = df[df["State"] == option1]
+with col2:
+    option2 = st.selectbox("Store #", stores["Store"], key=2)
+
 
 st.divider()
-st.write("You selected store:", option2, "in", option1)
 
-df = df[df['State'] == option1]
+#Print and display filtered Dataframe
+df = df[(df['State']==option1) & (df['Store']==option2)]
 st.write(df)
-
-
-
 st.divider()
-
-
 
 
 # Datatable with SQL server
