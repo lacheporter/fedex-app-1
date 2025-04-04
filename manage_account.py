95a97039c0bb0c1a1295a614b9443a5318dea392
lacheporter <114413176+lacheporter@users.noreply.github.com>
2025-04-04 16:49:57 +0000

    add employee data and fix inventory logic

--- manage_account.py
+++ manage_account.py
@@ -2,3 +2,13 @@ import streamlit as st
 
 st.title("Account Management")
 st.write("Welcome! Nothing to see here")
+
+import streamlit as st
+
+if not st.experimental_user.is_logged_in:
+    if st.button("Log in"):
+        st.login()
+else:
+    if st.button("Log out"):
+        st.logout()
+    st.write(f"Hello, {st.experimental_user.name}!")
--- manage_stores.py
+++ manage_stores.py
@@ -3,58 +3,64 @@ import requests
 import pandas as pd
 import streamlit as st
 
-data = [
-        {"Store": 440, "State": "New Jersey","Employees": 4, "Closed": True},
-        {"Store": 760, "State": "New Jersey","Employees": 4, "Closed": True},
-        {"Store": 430, "State": "South Carolina", "Employees": 5, "Closed": False},
-        {"Store": 250, "State": "South Carolina", "Employees": 3, "Closed": True},
-        {"Store": 360, "State": "South Carolina", "Employees": 3, "Closed": True},
-        {"Store": 420, "State": "Georgia", "Employees": 6, "Closed": True},
-        {"Store": 220, "State": "West Virginia", "Employees": 3, "Closed": True},
-        {"Store": 567, "State": "West Virginia", "Employees": 6, "Closed": True},
-        {"Store": 489, "State": "Iowa", "Employees": 4, "Closed": True},
-        {"Store": 440, "State": "North Carolina", "Employees": 6, "Closed": True},
-    ]
-
-st.title("Fedex Store Management")
-
-#Create a dataframe...
+#import data
+data = pd.read_json('data.json')
+# Create a dataframe...
 df = pd.DataFrame(data)
 
+#Create Title, Columns, Select boxes, drop duplicates...
+st.title("Fedex Store Management")
+st.subheader("Manage your store data.")
+st.write("Select your location and store to start editing!")
 col1, col2 = st.columns([2, 1])
-#Create Select box of States, drop duplicates...
 states = df["State"].drop_duplicates()
 with col1:
-    option1 = st.selectbox("Location",(states),key=1)
+    option1 = st.selectbox("Location", (states), key=1)
 
-#Create select box of Stores based on State choice...
+# Create select box of Stores based on State choice...
 stores = df[df["State"] == option1]
 with col2:
     option2 = st.selectbox("Store #", stores["Store"], key=2)
 
+st.write("You selected Store:", option2, "in", option1)
+st.divider()
+
+st.write("Store Information:")
+# Print and display filtered Dataframe
+df = df[(df['State'] == option1) & (df['Store'] == option2)]
+st.dataframe(df, column_config={"Employees": None, "Inventory": None})
+
+#create editable dataframe from inventory
+st.write("Inventory Information:")
+inventoryItems =[]
+for items in df["Inventory"]:
+    inventoryItems.append(items)
+st.data_editor(inventoryItems)
+
+st.write("Employee Information:")
+EmployeeItems =[]
+for items in df["Employees"]:
+    EmployeeItems.append(items)
+st.data_editor(items)
+st.divider()
+
+st.title("Import Store Data")
+st.write("Import CSV of your existing store information.")
 
 st.divider()
 
-#Print and display filtered Dataframe
-df = df[(df['State']==option1) & (df['Store']==option2)]
-st.write(df)
-st.divider()
-
-
-# Datatable with SQL server
-st.title("Test Api Call")
+#API request testing...
+st.title("Explore Store")
 st.write("Need a SQL Database to connect, using star wars api for now")
-
 try:
     data = requests.get("https://swapi.dev/api/planets/").json()
 except:
     print("no data")
-
 st.dataframe(
     data['results'],
     selection_mode="multi-row",
     column_config={
-        "name":"Planet",
-        "climate":"Weather"
+        "name": "Planet",
+        "climate": "Weather"
     },
 )
